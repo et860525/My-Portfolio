335bf1c71c02ff48fa710d850a5f81c71fd1fc00
et860525 <et860525@gmail.com>
2022-06-29 12:11:57 +0800

    Init project
      - Vue 3
      - Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <nav class="py-4 px-6 text-sm font-medium">
+    <ul class="flex space-x-3 justify-center">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <Home />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
